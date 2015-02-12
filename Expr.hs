3e786cc96702c1d5a11c25e815823ff13fa6b4b5
Insik Cho <iminsik@gmail.com>
2015-02-11 23:01:50 -0800

    Try to define 'FunE'

--- Expr.hs
+++ Expr.hs
@@ -35,10 +35,37 @@ data CExpr = NumC Integer
            deriving (Eq, Show)
 
 parseExpr :: SExp -> Result Expr
-parseExpr sexp = Err "parseExpr not implemented yet"
+parseExpr (NumS n)  = Ok(NumE n)
+parseExpr (IdS "true") = Ok(VarE "true")
+parseExpr (IdS "false") = Ok(VarE "false")
+-- ifs are 4 element lists
+parseExpr (ListS [IdS "if",cond,con,alt]) =
+  case parseExpr cond of
+    Ok(cond') -> case parseExpr con of
+      Ok(con') -> case parseExpr alt of
+        Ok(alt') -> Ok (IfE cond' con' alt')
+        Err(msg) -> Err msg
+      Err(msg) -> Err msg
+    Err(msg) -> Err msg
+
+parseExpr (ListS [IdS "fun",arg,expr]) =
+  case parseExpr arg of
+    Ok(arg')  -> case parseExpr expr of
+      Ok(expr') -> Ok(FunE arg' expr')
+      Err(msg)  -> Err msg
+    Err(msg)  -> Err msg
+
+-- anything else is an error
+parseExpr _ = Err "unrecognized expression"
+
+-- parseExprHelper :: SExp -> 
+-- parseExpr sexp = Err "parseExpr not implemented yet"
 
 desugar :: Expr -> Result CExpr
 desugar expr = Err "desugar not implemented yet"
 
 checkIds :: [String] -> [String] -> CExpr -> Result ()
 checkIds bound reserved expr = Err "checkIds not implemented yet"
+
+parseExprVal :: Result (SExp, [a]) -> Result Expr
+parseExprVal (Ok sexp@(t,ts)) = parseExpr (t)
